4f6e7046825d6cdf8d33a53a900f27b9fa9b84b1
j0y0nt <4onsocial@gmail.com>
2023-03-12 21:18:16 +0530

    Change default port.

--- routes/stonk.js
+++ routes/stonk.js
@@ -20,7 +20,7 @@ router.get('/', function(req, res, next) {
     const pageNumber = Number(req.query.page);
     const pageSize = Number(req.query.pageSize);
     const invalidParam = !isNaN(pageNumber) && !isNaN(pageSize);
-
+    
     if(!invalidParam) {
 	res.status(400).send('Bad query param: page=' + req.query.page
 			     + ' pageSize=' + req.query.pageSize)
@@ -28,7 +28,32 @@ router.get('/', function(req, res, next) {
 	const start = pageNumber * pageSize;
 	const end = pageSize;
 	console.log('Getting stock data from start index: ' + start + 'to end index: ' + end);
-	res.send(stocksData.slice(start, start + end));
+	const headers = stocksData.at(0).split(",");
+	console.log(typeof headers);
+	const result = stocksData.slice(start+1, start + end+1);
+	const jsonArray = [];
+	
+	result.map((item) => {
+	    const secData = item.split(",");
+	    let stockItem = {};
+	    try {
+		headers.map((h, idx) => {
+		    //console.log(h + ' yo ');//+ idx);
+		    if(h === 'SC_CODE') {
+			stockItem['id'] = secData[idx];
+		    } else {
+			stockItem[h] = secData[idx];
+		    }
+		});
+
+	    } catch (error) {
+		console.error(error);
+		// Expected output: ReferenceError: nonExistentFunction is not defined
+		// (Note: the exact output may be browser-dependent)
+	    }
+	    jsonArray.push(stockItem);
+	});
+	res.send(jsonArray);
     }
 });
 
